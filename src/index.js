eb63f2dbe9f39d94502cb522b379368554c73476
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-05 15:32:51 +0200

    Import `myName` module into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  console.log(myName('Dylan'));
 
   return element;
 }
