f2b353ba537669fb5814fd9b550008b5ec0c3674
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-05 15:59:27 +0200

    Import `style.css` into index.js module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
 import myName from './myName';
 import { functionOne, functionTwo } from './myModule';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  
   console.log(myName('Dylan'));
   console.log(functionOne());
   console.log(functionTwo());
