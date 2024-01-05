89d6163fe8989fadeed1f7d6ddc06afb537b7589
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-05 17:58:42 +0200

    Import image into index.js module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import myName from './myName';
 import { functionOne, functionTwo } from './myModule';
 import './style.css';
+import Icon from './icon.svg';
 
 function component() {
   const element = document.createElement('div');
@@ -9,7 +10,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-  
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
   console.log(myName('Dylan'));
   console.log(functionOne());
   console.log(functionTwo());
