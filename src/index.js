38585f453624b7f00c3d46f9363e94aa0636f805
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-05 15:38:04 +0200

    Import functionOne and functionTwo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import myName from './myName';
+import { functionOne, functionTwo } from './myModule';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,8 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   console.log(myName('Dylan'));
+  console.log(functionOne());
+  console.log(functionTwo());
 
   return element;
 }
