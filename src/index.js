9399894c844be46ab10afdafa24543f8bf1b0431
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-06 11:00:35 +0200

    Add toml, yaml and json5 parsers to webpack config

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import myName from './myName';
 import { functionOne, functionTwo } from './myModule';
 import './style.css';
 import Icon from './icon.png';
+
 import Data from './data.xml';
 import Notes from './data.csv';
 import Data2 from './data.json';
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title);
+console.log(toml.owner.name);
+
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+console.log(json.title);
+console.log(json.owner.name);
 
 function component() {
   const element = document.createElement('div');
