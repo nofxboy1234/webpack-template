850011b5ee4bf648523804276919f4fa73e46534
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-03 12:10:59 +0200

    Clean up removed dependencies in webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,5 @@
 const path = require('path');
 
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -37,35 +33,6 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
       },
     ],
   },
