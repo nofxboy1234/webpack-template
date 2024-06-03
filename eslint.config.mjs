f1f0b555998d7e07df6735084399e1177d06d60f
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-03 13:14:05 +0200

    Copy prettier and eslint configs from vscode-eslint-example repo

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from 'globals';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+
+import eslintConfigPrettier from 'eslint-config-prettier';
+import stylistic from '@stylistic/eslint-plugin';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('airbnb-base'),
+  eslintConfigPrettier,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/quotes': ['error', 'single'],
+    },
+  },
+];
