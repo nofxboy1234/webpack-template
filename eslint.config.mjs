dc78e8bf34e4865a595ec5132909946efa97ff82
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-03 21:01:37 +0200

    Install eslint locally
    npm init @eslint/config@latest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,8 @@
-import globals from 'globals';
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
-
-import eslintConfigPrettier from 'eslint-config-prettier';
-import stylistic from '@stylistic/eslint-plugin';
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-});
 
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.browser } },
-  ...compat.extends('airbnb-base'),
-  eslintConfigPrettier,
-  {
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      '@stylistic/quotes': ['error', 'single'],
-    },
-  },
-  {
-    ignores: ['**/node_modules/', '**/dist/', 'package-lock.json'],
-  },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
 ];
